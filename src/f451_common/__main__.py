bbd2a2b2434a7f7a6df1f83bf36c273b1e7fdff1
Martin Lanser <martinlanser@gmail.com>
2023-11-17 09:29:54 -0500

    update demo

--- src/f451_common/__main__.py
+++ src/f451_common/__main__.py
@@ -1,15 +1,22 @@
 """Demo for using f451 Labs Common Module."""
 
-import f451_common
+from pathlib import Path
+import f451_common.common as f451Common
 
 # =========================================================
 #                    D E M O   A P P
 # =========================================================
 def main():
-    print("\n====== [Demo of f451 Labs Common module] ======")
-    print("Hello world!")
+    APP_DIR = Path(__file__).parent         # Find dir for this app
+    settingsFile = "demo.toml"
+    settings = f451Common.load_settings(APP_DIR.joinpath(settingsFile))
 
-    print("=============== [End of Demo] =================\n")
+    print("\n====== [Demo of f451 Labs Common module] ======")
+    print(f"Reading values from '{settingsFile}' file:\n")
+    for k, v in settings.items():
+        print(f"  {k} = {v}")
+
+    print("\n=============== [End of Demo] =================\n")
 
 
 if __name__ == "__main__":
